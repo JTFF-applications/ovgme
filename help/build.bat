752523c6246410c58c6c1eb097a2af9bf600f549
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-06-09 11:38:43 +0200

    Setup: Remove InooSetup dependency, Add help build as post build command

--- help/build.bat
+++ help/build.bat
@@ -1,2 +1,11 @@
-CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" en\ovgme.hhp
-CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" fr\ovgme.hhp
+@echo off
+
+if exist %1\ (
+	pushd %1%
+		CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" en\ovgme.hhp
+		CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" fr\ovgme.hhp
+	popd
+) else (
+	CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" en\ovgme.hhp
+	CALL "C:\Program Files (x86)\HTML Help Workshop\hhc.exe" fr\ovgme.hhp
+)
